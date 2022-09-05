8664382ef543a63e4606be24d0470306056183ec
EvgenyGersh <evg.gershberg@gmail.com>
2022-09-05 23:54:17 +0300

    commit-7

--- js/app.js
+++ js/app.js
@@ -1,60 +1,72 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const mainSwiper = new Swiper(".interface__slider", {
-    effect: "cards",
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	const mainSwiper = new Swiper('.interface__slider',{
+		
+		effect: "cards",
     grabCursor: true,
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+          el: ".swiper-pagination",
+          clickable: true,
+          },
+	})
 
-  var slidelength = document.querySelectorAll(".swiper-slide").length;
-  var custSpan = document.querySelectorAll(".cust-sp");
-  var sliderWidth = document.querySelector(".interface__slider").clientWidth;
-  var custSpanWidth = sliderWidth / slidelength - 12;
-  var cusBullet = document.querySelectorAll(".swiper-pagination-bullet");
+  var slidelength = document.querySelectorAll('.swiper-slide').length;
+  var custSpan = document.querySelectorAll('.cust-sp');
+  var sliderWidth = document.querySelector('.interface__slider').clientWidth;
+  var custSpanWidth = ((sliderWidth / slidelength) - 12);
+  var cusBullet = document.querySelectorAll('.swiper-pagination-bullet');
 
-  cusBullet.forEach(function (item) {
-    item.style.width = custSpanWidth + "px";
-  });
+      cusBullet.forEach(function(item){
+        item.style.width = custSpanWidth + 'px';
+      })
+  
 
-  document.querySelector(".logo-link").addEventListener("click", function (e) {
+
+
+  document.querySelector('.logo-link').addEventListener('click',function(e){
     e.preventDefault();
     // console.log('click');
+    
+    mainSwiper.slideTo( 0, 500, false );
+  })
 
-    mainSwiper.slideTo(0, 500, false);
-  });
-
-  document.querySelector(".tabs-link").addEventListener("click", function (e) {
+  document.querySelector('.tabs-link').addEventListener('click',function(e){
     e.preventDefault();
-    let mainCont = document.querySelector(".main-cont");
+    let mainCont = document.querySelector('.main-cont');
+     
+      mainCont.classList.remove('passive'); 
+      mainCont.classList.add('passive');
+      document.querySelector('.tab-item').classList.add('active');
+  })
 
-    mainCont.classList.remove("passive");
-    mainCont.classList.add("passive");
-    document.querySelector(".tab-item").classList.add("active");
-  });
-
-  document.querySelector(".tgl-main").addEventListener("click", function (e) {
+  document.querySelector('.tgl-main').addEventListener('click',function(e){
     e.preventDefault();
-    let mainCont = document.querySelector(".main-cont");
+    let mainCont = document.querySelector('.main-cont');
+     
+      mainCont.classList.remove('passive'); 
+      
+    
+  })
 
-    mainCont.classList.remove("passive");
-  });
+  
+  const tabsBtn = document.querySelectorAll('.tab-lnk');
+  const tabsItems = document.querySelectorAll('.tab-item');
+    
+    tabsBtn.forEach(function(item){
+      item.addEventListener('click', function(e){
+        e.preventDefault();
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute('data-tab');
+        let currentTab = document.querySelector(tabId);
 
-  const tabsBtn = document.querySelectorAll(".tab-lnk");
-  const tabsItems = document.querySelectorAll(".tab-item");
-
-  tabsBtn.forEach(function (item) {
-    item.addEventListener("click", function (e) {
-      e.preventDefault();
-      let currentBtn = item;
-      let tabId = currentBtn.getAttribute("data-tab");
-      let currentTab = document.querySelector(tabId);
-
-      tabsItems.forEach(function (item) {
-        item.classList.remove("active");
-      });
-      currentTab.classList.add("active");
-    });
-  });
-});
+        tabsItems.forEach(function(item){
+          item.classList.remove('active');
+        });
+        currentTab.classList.add('active');
+        
+      })
+    })
+    
+})
